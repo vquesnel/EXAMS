c90c03288852042ee3c73293ee29fd5ded44943a
Victor QUESNEL <vquesnel@e2r1p6.42.fr>
2016-04-10 16:20:50 +0200

    push

--- EXAM_C_BEGINNER/5-1-ft_itoa_base/ft_itoa_base.c
+++ EXAM_C_BEGINNER/5-1-ft_itoa_base/ft_itoa_base.c
@@ -6,82 +6,80 @@
 /*   By: vquesnel <vquesnel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/09 22:45:23 by vquesnel          #+#    #+#             */
-/*   Updated: 2016/03/09 22:46:11 by vquesnel         ###   ########.fr       */
+/*   Updated: 2016/04/10 16:19:05 by vquesnel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
+#include "stdlib.h"
+#include "stdio.h"
 
-int		int_len(int n, int base)
+int		int_len(int nb, int base)
 {
-	int		len;
+	int i;
 
-	len = 0;
-	if (n < 0 && base == 10)
+	i = 0;
+	if (nb < 0 && base == 10)
 	{
-		n = -n;
-		len++;
+		++i;
+		nb = -nb;
 	}
-	if (n < 0 && base != 10)
-		n = -n;
-	while (n > 0)
+	if (nb < 0 && base != 10)
+		nb =-nb;
+	while (nb)
 	{
-		n = n / 10;
-		len++;
+		nb /= base;
+		++i;
 	}
-	return (len);
+	return (i);
 }
 
-int		max_power(int value, int base)
+long	max_power(long nb, int base)
 {
-	int		power;
+	long max;
 
-	power = base;
-	if (value < 0)
-		value = -value;
-	while (power < value)
-		power = power * base;
-	if (power > value)
-		power = power / base;
-	return (power);
+	max = base;
+	while (max < nb)
+		max *= base;
+	if (max > nb)
+		max /= base;
+	return (max);
 }
 
 char	*ft_itoa_base(int value, int base)
 {
-	int		power;
-	int		index1;
-	int		index2;
-	int		rest;
-	int		len;
-	char	hex[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-	char	*toa;
+	long	nb;
+	long	max;
+	char	hex[16] = "0123456789ABCDEF";
+	char	*text;
+	int		i;
+	int		j;
 
-	if (base < 2 || base > 16)
-		return (0);
-	power = max_power(value, base);
-	len = int_len(value, base);
-	toa = malloc(len + 1);
-	index2 = 0;
-	if (value < 0 && base == 10)
+	if ((text = (char *)malloc(sizeof(char) * int_len(value, base) + 1)) == NULL)
+		return (NULL);
+	nb = (long)value;
+	i = 0;
+	j = 0;
+	if (nb < 0 && base == 10)
 	{
-		toa[index2] = '-';
-		value = -value;
-		index2++;
+		text[i]= '-';
+		i++;
+		nb = -nb;
 	}
-	if (value < 0 && base != 10)
-		value = -value;
-	while (power > 0)
+	if (nb < 0 && base != 10)
 	{
-		index1 = value / power;
-		rest = value % power;
-		toa[index2] = hex[index1];
-		power = power / base;
-		value = rest;
-		index2++;
+		nb = -nb;
 	}
-	toa[index2] = '\0';
-	return (toa);
+	max = max_power(nb, base);
+	while (max > 0)
+	{
+		j = nb / max;
+		text[i] = hex[j];
+		nb %= max;
+		max /= base;
+		i++;
+	}
+	text[i] = '\0';
+	return (text);
 }
 
 int		main(int ac, char **av)
@@ -99,4 +97,3 @@ int		main(int ac, char **av)
 	}
 	return (0);
 }
-
